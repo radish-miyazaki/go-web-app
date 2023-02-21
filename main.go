abfb70cecce4294eacae2173465d7a02cc4ec9f3
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-02-21 15:09:08 +0900

    refactor main run method

--- main.go
+++ main.go
@@ -5,21 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/radish-miyazaki/go-web-app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
-	// シグナルを受け取ったら、グレースシャットダウンを開始する
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -32,34 +23,9 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// FIXME: コマンドラインで一時的に確認するため
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-
-	// 別ゴルーチンでサーバを起動
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		// http.ErrServerClosed は http.Server.Shutdown() が正常に終了したことを示すので異常ではない
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	// チャネルからの通知（終了通知）を受け取る
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-
-	// Goメソッドで起動した別ゴルーチンの終了を待つ
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
 
 func main() {

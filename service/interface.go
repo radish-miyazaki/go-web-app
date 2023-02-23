5e4a62b320144958dc760ea8a1dec81d3a2c4423
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-02-23 19:19:11 +0900

    add register user service impl

--- service/interface.go
+++ service/interface.go
@@ -7,11 +7,15 @@ import (
 	"github.com/radish-miyazaki/go-web-app/store"
 )
 
-//go:generate go run github.com/matryer/moq -out moq_test.go . TaskAdder TaskListener
+//go:generate go run github.com/matryer/moq -out moq_test.go . TaskAdder TaskListener UserRegister
 type TaskAdder interface {
 	AddTask(ctx context.Context, db store.Execer, t *entity.Task) error
 }
 
 type TaskLister interface {
 	ListTasks(ctx context.Context, db store.Queryer) (entity.Tasks, error)
+}
+
+type UserRegister interface {
+	RegisterUser(ctx context.Context, db store.Execer, u *entity.User) error
 }
---
+++ service/register_user.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/radish-miyazaki/go-web-app/entity"
+	"github.com/radish-miyazaki/go-web-app/store"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type RegisterUser struct {
+	DB   store.Execer
+	Repo UserRegister
+}
+
+func (ru *RegisterUser) RegisterUser(ctx context.Context, name, password, role string) (*entity.User, error) {
+	pw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("cannot hash password: %w", err)
+	}
+	u := &entity.User{
+		Name:     name,
+		Password: string(pw),
+		Role:     role,
+	}
+	if err := ru.Repo.RegisterUser(ctx, ru.DB, u); err != nil {
+		return nil, fmt.Errorf("failed to register: %w", err)
+	}
+	return u, nil
+}
